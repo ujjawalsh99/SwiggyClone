30a0dab3d6ee423f76697b576f4176b010a36f15
Ujjawal Shrivastava <ujjawalsh99@gmail.com>
2023-12-01 17:57:01 +0530

    EP-05 Top Rated Restaurant implementation

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,31 @@
 import RestaurantData from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 
 const Body = () => {
+  const [restaurantDataList, setRestaurantDataList] = useState(RestaurantData);
+
+  function getTopRatedRestaurants() {
+    let filterRestaurantData = RestaurantData.filter((item) => {
+      return item?.info?.avgRating > 4;
+    });
+    setRestaurantDataList(filterRestaurantData);
+  }
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={function () {
+            getTopRatedRestaurants();
+          }}
+        >
+          Top rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {RestaurantData.map((data) => {
+        {restaurantDataList.map((data) => {
           return <RestaurantCard key={data?.info?.id} resData={data?.info} />;
         })}
       </div>
@@ -15,3 +34,7 @@ const Body = () => {
 };
 
 export default Body;
+
+function execute() {
+  console.log("Bhakti he shakti hai");
+}
